f090628b36268d346214b94d12aaa8584e179e47
paulonteri <onteripaul@gmail.com>
2020-01-17 13:20:03 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World! From Paul Onteri and React.</h1>
+    </div>
+  );
+}
+
+export default App;
