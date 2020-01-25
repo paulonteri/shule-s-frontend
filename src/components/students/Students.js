6356ac6f73180b75057727e9d4370878066d8e28
paulonteri <onteripaul@gmail.com>
2020-01-25 16:38:24 +0300

    getStudents action working

---
+++ src/components/students/Students.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from "react";
+
+import Studentstable from "./Studentstable";
+
+export class Students extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Studentstable />
+      </Fragment>
+    );
+  }
+}
+
+export default Students;
