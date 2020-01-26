27d4a25fb93ec26fc68db399b5af39d9909dd60b
paulonteri <onteripaul@gmail.com>
2020-01-26 19:12:16 +0300

    Added nested routes

--- src/components/library/Library.js
+++ src/components/library/Library.js
@@ -1,12 +1,20 @@
-import LibraryDashboard from "./LibraryDashboard";
-
 import React, { Component, Fragment } from "react";
+import { Link, Route, Switch } from "react-router-dom";
+
+import LibraryDashboard from "./LibraryDashboard";
+import BooksTable from "./BooksTable";
 
 export class Library extends Component {
   render() {
     return (
       <Fragment>
-        <LibraryDashboard />
+        {/* <LibraryDashboard /> */}
+        <div>
+          <Switch>
+            <Route exact path="library/" component={LibraryDashboard} />
+            <Route exact path="/library/bookstable" component={BooksTable} />
+          </Switch>
+        </div>
       </Fragment>
     );
   }
