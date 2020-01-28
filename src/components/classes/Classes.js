4c25e1decdd0a053ae75a19bae293bc1a3076c8a
paulonteri <onteripaul@gmail.com>
2020-01-28 06:39:13 +0300

    Added class numeral list to frontend

--- src/components/classes/Classes.js
+++ src/components/classes/Classes.js
@@ -5,6 +5,7 @@ import ClassesDashboard from "./ClassesDashboard";
 import StreamList from "./StreamList";
 import StreamForm from "./StreamForm";
 import Streams from "./Streams";
+import ClassNList from "./ClassNList";
 
 export class Classes extends Component {
   render() {
@@ -12,6 +13,11 @@ export class Classes extends Component {
       <Fragment>
         <Switch>
           <Route exact path="/classes" component={ClassesDashboard} />
+          <Route
+            exact
+            path="/classes/classnumeralslist"
+            component={ClassNList}
+          />
           <Route exact path="/classes/streams" component={Streams} />
           <Route exact path="/classes/streamlist" component={StreamList} />
           <Route exact path="/classes/streamform" component={StreamForm} />
