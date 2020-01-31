b02da913c563cbefd21900dcf00443276db147f9
paulonteri <onteripaul@gmail.com>
2020-01-31 18:29:00 +0300

    fixed bug in dormitories reducer

--- src/reducers/dormitories/dormitories.js
+++ src/reducers/dormitories/dormitories.js
@@ -11,7 +11,7 @@ export default function(state = initialState, action) {
     case GET_DORMS:
       return {
         ...state,
-        classes: action.payload
+        dormitories: action.payload
       };
     default:
       return state;
