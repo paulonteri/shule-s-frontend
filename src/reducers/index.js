7fa2fdf7d257235ea509f34aa84de56a3316cbf8
paulonteri <onteripaul@gmail.com>
2020-01-31 10:07:18 +0300

    fixed bug in classes reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,12 +5,14 @@ import messages from "./messages";
 import students from "./students/students";
 import streams from "./classes/streams";
 import classNumerals from "./classes/classNumeral";
+import classes from "./classes/classes";
 
 export default combineReducers({
   errorsReducer: errors,
   messagesReducer: messages,
   booksReducer: books,
   studentsReducer: students,
+  classesReducer: classes,
   streamsReducer: streams,
   classNumeralsReducer: classNumerals
 });
