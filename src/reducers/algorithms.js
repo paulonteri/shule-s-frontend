e5c53b05dc154a45edd8b58d644a8bf261eb28e4
paulonteri <onteripaul@gmail.com>
2020-04-16 12:53:00 +0300

    Saved reducer's algorithms to a different file

---
+++ src/reducers/algorithms.js
@@ -0,0 +1,14 @@
+export function findAndReplace(old_array, new_object) {
+    const myNewArray = old_array.filter(function(obj) {
+        return obj.id !== new_object.id;
+    });
+    myNewArray.push(new_object);
+    return myNewArray;
+}
+
+export function deleteObject(old_array, deleted_object_id) {
+    const dataAfterDelete = old_array.filter(
+        obj => obj.id != deleted_object_id
+    );
+    return dataAfterDelete;
+}
