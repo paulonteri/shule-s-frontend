92e7e38ac478ddd738aa9c8407d400749d74274f
paulonteri <onteripaul@gmail.com>
2020-04-28 12:25:04 +0300

    Added number per class chart

---
+++ src/components/students/analytics/NumberPerClass.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Doughnut } from "react-chartjs-2";
+import useStudentAnalyticsGeneral from "../../../hooks/useStudentAnalyticsGeneral";
+import { backgroundColor, hoverBackgroundColor } from "../../../layout/colors";
+
+export const NumberPerClass = props => {
+    useStudentAnalyticsGeneral();
+    const [classes, setClasses] = useState(null);
+    const [totals, setTotals] = useState(null);
+
+    useEffect(() => {
+        if (props.studentsPerClass != undefined) {
+            setClasses(
+                props.studentsPerClass.map(
+                    cl => `${cl.class_ns__class_numeral} ${cl.class_ns__stream}`
+                )
+            );
+            setTotals(props.studentsPerClass.map(cl => cl.Total));
+        }
+    }, [props.studentsPerClass]);
+    console.log(classes, totals);
+
+    const data = {
+        labels: classes,
+        datasets: [
+            {
+                data: totals,
+                backgroundColor: backgroundColor,
+                hoverBackgroundColor: hoverBackgroundColor
+            }
+        ]
+    };
+
+    const legendOpts = {
+        display: true,
+        position: "bottom",
+        fullWidth: false,
+        reverse: true
+    };
+
+    return (
+        <div>
+            <Doughnut width={100} height={50} data={data} legend={legendOpts} />
+        </div>
+    );
+};
+
+NumberPerClass.propTypes = {
+    studentsPerClass: PropTypes.array.isRequired
+};
+
+const mapStateToProps = state => ({
+    studentsPerClass:
+        state.studentAnalyticsReducer.studentAnalytics.students_per_class
+});
+
+export default connect(mapStateToProps)(NumberPerClass);
