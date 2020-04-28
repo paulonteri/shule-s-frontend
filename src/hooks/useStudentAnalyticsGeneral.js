0fe67777eab97026f8b2424d92941cbec7dd525f
paulonteri <onteripaul@gmail.com>
2020-04-28 12:12:21 +0300

    Added get Student analytics hook

---
+++ src/hooks/useStudentAnalyticsGeneral.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux/es/hooks/useDispatch";
+import { getStudentAnalytics } from "../actions/students/analytics/students";
+
+function useStudentAnalyticsGeneral() {
+    const dispatch = useDispatch();
+    const [time, setTime] = useState(null);
+
+    useEffect(() => {
+        const today = new Date();
+
+        if (!(today.getMinutes() - time < 1)) {
+            dispatch(getStudentAnalytics());
+            console.log("awesome");
+            setTime(today.getMinutes());
+        } else {
+            console.log("pass");
+        }
+    }, []);
+}
+
+export default useStudentAnalyticsGeneral;
