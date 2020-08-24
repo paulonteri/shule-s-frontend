86228e2b1de052ff42a38080a175172f3277e441
Paul Onteri <onteripaul@gmail.com>
2020-08-24 17:08:16 +0300

    Add sentry

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-// import "antd/dist/antd.css";
-import App from "./App";
+import * as Sentry from "@sentry/react";
 import * as serviceWorker from "./serviceWorker";
+import App from "./App";
+
+Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    environment: process.env.NODE_ENV
+});
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
