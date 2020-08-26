53c3773c637c2804cd81dcab656bf4319c1787e1
Paul Onteri <onteripaul@gmail.com>
2020-08-26 11:30:34 +0300

    simple tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import ReactDOM from "react-dom";
 import App from "./App";
+import { shallow } from "enzyme";
+import { render } from "@testing-library/react";
 
-test("renders learn react link", () => {
+it("<App /> shallow renders without crashing", () => {
+    shallow(<App />);
+});
+
+it("<App /> renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+});
+
+it("<App /> loading screen", () => {
     const { getByText } = render(<App />);
-    const linkElement = getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+    expect(
+        getByText("Establishing a secure connection...")
+    ).toBeInTheDocument();
 });
