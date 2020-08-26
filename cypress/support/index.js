832aad91121d6064f335c7b61a40830789ec2380
Paul Onteri <onteripaul@gmail.com>
2020-08-27 01:28:28 +0300

    cypress configs

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,17 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on("uncaught:exception", (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    // Ignore firebase error
+    expect(err.message).to.include(
+        "Uncaught SyntaxError: Unexpected token '<'"
+    );
+    return false;
+});
