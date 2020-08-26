cda60cf96c7b2bdb5241ec7aebd39a05eaf642ef
Paul Onteri <onteripaul@gmail.com>
2020-08-26 21:54:14 +0300

    install cypress

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { shallow } from "enzyme";
 import { render } from "@testing-library/react";
+/// <reference types="cypress" />
+import "cypress-react-selector";
+import { mount } from "cypress-react-unit-test";
 
 it("<App /> shallow renders without crashing", () => {
     shallow(<App />);
@@ -18,4 +21,38 @@ it("<App /> loading screen", () => {
     expect(
         getByText("Establishing a secure connection...")
     ).toBeInTheDocument();
+});
+
+it("renders without crashing", () => {
+    cy.stub(window, "fetch")
+        .withArgs("http://myapi.com/products")
+        .resolves({
+            json: cy.stub().resolves({
+                products: [
+                    { id: 1, name: "First item" },
+                    { id: 2, name: "Second item" }
+                ]
+            })
+        });
+    // mount the react component
+    mount(<ProductsList />);
+    cy.contains("First item").should("be.visible");
+    cy.get(".product").should("have.length", 2);
+
+    // use cypress-react-selector
+    cy.waitForReact(1000, "#cypress-root");
+    cy.react("ProductsContainer").should("have.class", "product-container");
+    cy.react("AProduct").should("have.length", 2);
+    cy.react("AProduct", { name: "Second item" })
+        .should("be.visible")
+        .and("have.text", "Second item");
+    cy.getReact("AProduct", { name: "Second item" })
+        .getProps()
+        .should("have.property", "name");
+    cy.getReact("AProduct", { name: "First item" })
+        .getProps("name")
+        .should("eq", "First item");
+    cy.getReact("AProduct", { name: "Second item" })
+        .getCurrentState()
+        .should("not.empty");
 });
