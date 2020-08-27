902d5ab6eac6ec38dd95d6de580a91e2d6ce64bd
Paul Onteri <onteripaul@gmail.com>
2020-08-27 13:05:01 +0300

    Added more tests

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,20 +1,33 @@
 const SERVER_URL = "http://localhost:8000";
 const API_URL = SERVER_URL + "/api/v2.0";
 
-describe("Home Page", () => {
-    it("Home Page works", () => {
+describe("Smoke Test", () => {
+    it("Smoke Test", () => {
         cy.visit("/");
     });
 });
 
 describe("Login", () => {
-    it("Login Page works", () => {
-        cy.visit("/#/login");
+    it("Must Login to access pages", () => {
+        cy.visit("/#/students", {
+            onBeforeLoad: win => {
+                win.sessionStorage.clear();
+            }
+        });
+        cy.contains("Kindly login");
+        cy.url().should("eq", Cypress.config().baseUrl + "/#/login");
     });
+
+    it("Login Page works", () => {
+        cy.visit("/#/login", {
+            onBeforeLoad: win => {
+                win.sessionStorage.clear();
+            }
+        });
+    });
+
     it("Login Page elements", () => {
-        cy.window().then(win => {
-            win.sessionStorage.clear();
-            cy.clearLocalStorage();
+        cy.clearLocalStorage().then(() => {
             cy.get("#email");
             cy.get("#password");
             cy.get(".ant-btn");
@@ -22,9 +35,7 @@ describe("Login", () => {
     });
 
     it("Login data submission", () => {
-        cy.window().then(win => {
-            win.sessionStorage.clear();
-            cy.clearLocalStorage();
+        cy.clearLocalStorage().then(() => {
             cy.server();
 
             cy.get("#email").clear();
@@ -49,18 +60,97 @@ describe("Login", () => {
 describe("Student", () => {
     beforeEach(() => {
         cy.server();
+        cy.route("GET", API_URL + "/students/students/").as("getStudents");
+        cy.route("GET", API_URL + "/academics/classes").as("getClasses");
+        cy.route("GET", API_URL + "/dormitories").as("getDormitories");
     });
 
-    it("Student Home", () => {
+    it("Dashboard", () => {
         cy.visit("/#/students");
+        cy.get(":nth-child(1) > .container");
+        cy.get(":nth-child(2) > .container");
+        cy.get(":nth-child(3) > .container");
+        cy.get(".col > .container-fluid");
         cy.url().should("eq", Cypress.config().baseUrl + "/#/students");
     });
-    it("Student Add", () => {
+
+    it("Student Add Page", () => {
         cy.visit("/#/students/add");
+        cy.get("#first_name");
+        cy.get("h5");
         cy.url().should("eq", Cypress.config().baseUrl + "/#/students/add");
     });
-    it("Student View", () => {
+
+    it("Student Table Page", () => {
         cy.visit("/#/students/table");
+        cy.get(".table-responsive");
+        cy.wait("@getStudents").then(xhr => {
+            assert.equal(xhr.status, 200);
+            assert.isNotNull(
+                xhr.response.body.data,
+                "Students API call has data"
+            );
+        });
+        cy.wait("@getClasses").then(xhr => {
+            assert.equal(xhr.status, 200);
+            assert.isNotNull(
+                xhr.response.body.data,
+                "Classes API call has data"
+            );
+        });
+        cy.wait("@getDormitories").then(xhr => {
+            assert.equal(xhr.status, 200);
+            assert.isNotNull(
+                xhr.response.body.data,
+                "Dormitories API call has data"
+            );
+        });
         cy.url().should("eq", Cypress.config().baseUrl + "/#/students/table");
+    });
+});
+
+describe("Library", () => {
+    beforeEach(() => {
+        cy.server();
+        cy.route("GET", API_URL + "/library/books").as("getBooks");
+        cy.route("GET", API_URL + "/library/booksnum").as("getBooksNum");
+        cy.route("GET", API_URL + "/academics/subject").as("getSubjects");
+    });
+
+    it("Dashboard", () => {
+        cy.visit("/#/library");
+        // analytics cards
+        cy.get(
+            ":nth-child(1) > .ant-card > .ant-card-head > .ant-card-head-wrapper > .ant-card-head-title"
+        );
+        // book list table
+        cy.get(".table-responsive > h4");
+        // Add book form
+        cy.get(".rounded > h4");
+        // Add book title form
+        cy.get(":nth-child(2) > h4");
+        // At least one book in the table
+        cy.get('[data-row-key="1"] > :nth-child(1) > a');
+
+        // API Calls
+        cy.wait("@getBooks").then(xhr => {
+            assert.equal(xhr.status, 200);
+            assert.isNotNull(xhr.response.body.data, "Books API call has data");
+        });
+        cy.wait("@getBooksNum").then(xhr => {
+            assert.equal(xhr.status, 200);
+            assert.isNotNull(
+                xhr.response.body.data,
+                "BooksNum API call has data"
+            );
+        });
+        cy.wait("@getSubjects").then(xhr => {
+            assert.equal(xhr.status, 200);
+            assert.isNotNull(
+                xhr.response.body.data,
+                "getSubjects API call has data"
+            );
+        });
+        cy.url().should("eq", Cypress.config().baseUrl + "/#/library");
     });
 });
